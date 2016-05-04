1bf87e2a357d1ad8a4e39af5f78ba4a5d9552471
Alex Kost <alezost@gmail.com>
2016-05-04 10:54:39 +0300

    Rename to "emms-state" (Fix #1)

--- emms-state.el
+++ emms-state.el
@@ -1,12 +1,12 @@
-;;; emms-status.el --- Display track description and playing time in the mode line
+;;; emms-state.el --- Display track description and playing time in the mode line
 
-;; Copyright © 2015 Alex Kost
+;; Copyright © 2015, 2016 Alex Kost
 
 ;; Author: Alex Kost <alezost@gmail.com>
 ;; Created: 22 Jan 2015
 ;; Version: 0.1
 ;; Package-Requires: ((emms "0"))
-;; URL: https://github.com/alezost/emms-status.el
+;; URL: https://github.com/alezost/emms-state.el
 ;; Keywords: emms
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -24,22 +24,21 @@
 
 ;;; Commentary:
 
-;; This package provides a minor mode (`emms-status-mode') for
-;; displaying description and playing time of the current track (played
-;; by EMMS) in the mode line.  A typical mode line string would look
-;; like this (it may be configured with `emms-status-mode-line-string'
-;; variable):
+;; This package provides a minor mode (`emms-state-mode') for displaying
+;; description and playing time of the current track (played by EMMS) in
+;; the mode line.  A typical mode line string would look like this (it
+;; may be configured with `emms-state-mode-line-string' variable):
 ;;
 ;;   ⏵ 1:19(5:14) Chopin - Waltz in a-moll, Op.34 No.2
 
 ;; To install the package manually, add the following to your init file:
 ;;
-;;   (add-to-list 'load-path "/path/to/emms-status-dir")
-;;   (autoload 'emms-status-mode "emms-status" nil t)
+;;   (add-to-list 'load-path "/path/to/emms-state-dir")
+;;   (autoload 'emms-state-mode "emms-state" nil t)
 
 ;; This package is intended to be used instead of `emms-mode-line' and
 ;; `emms-playing-time' modes and it is strongly recommended to disable
-;; these modes before enabling `emms-status-mode' (keep in mind that
+;; these modes before enabling `emms-state-mode' (keep in mind that
 ;; these modes are enabled automatically if you use `emms-all' or
 ;; `emms-devel' setup function).
 
@@ -48,64 +47,64 @@
 (require 'emms-mode-line)
 (require 'emms-playing-time)
 
-(defgroup emms-status nil
+(defgroup emms-state nil
   "Display track description and playing time in the mode line."
   :group 'emms)
 
-(defface emms-status-title
+(defface emms-state-title
   '((t nil))
   "Face used for the title of the current track."
-  :group 'emms-status)
+  :group 'emms-state)
 
-(defface emms-status-total-playing-time
+(defface emms-state-total-playing-time
   '((t :inherit font-lock-constant-face))
   "Face used for the total playing time."
-  :group 'emms-status)
+  :group 'emms-state)
 
-(defface emms-status-current-playing-time
+(defface emms-state-current-playing-time
   '((t :inherit font-lock-variable-name-face))
   "Face used for the current playing time."
-  :group 'emms-status)
+  :group 'emms-state)
 
-(defcustom emms-status-play "⏵"
+(defcustom emms-state-play "⏵"
   "String used to denote the 'play' state."
   :type 'string
-  :group 'emms-status)
+  :group 'emms-state)
 
-(defcustom emms-status-pause "⏸"
+(defcustom emms-state-pause "⏸"
   "String used to denote the 'pause' state."
   :type 'string
-  :group 'emms-status)
+  :group 'emms-state)
 
-(defcustom emms-status-stop "⏹"
+(defcustom emms-state-stop "⏹"
   "String used to denote the 'stop' state."
   :type 'string
-  :group 'emms-status)
+  :group 'emms-state)
 
-(defvar emms-status-mode-line-string
-  '(" " emms-status-state " "
-    (emms-status-current-playing-time
-     (:propertize emms-status-current-playing-time
-                  face emms-status-current-playing-time))
-    (emms-status-total-playing-time
+(defvar emms-state-mode-line-string
+  '(" " emms-state " "
+    (emms-state-current-playing-time
+     (:propertize emms-state-current-playing-time
+                  face emms-state-current-playing-time))
+    (emms-state-total-playing-time
      ("("
-      (:propertize emms-status-total-playing-time
-                   face emms-status-total-playing-time)
+      (:propertize emms-state-total-playing-time
+                   face emms-state-total-playing-time)
       ")"))
     emms-mode-line-string)
   "Mode line string with the EMMS info.")
-(put 'emms-status-mode-line-string 'risky-local-variable t)
+(put 'emms-state-mode-line-string 'risky-local-variable t)
 
-(defvar emms-status-state nil
+(defvar emms-state nil
   "Mode line construct for the state of the current EMMS process.")
 
-(defvar emms-status-current-playing-time nil
+(defvar emms-state-current-playing-time nil
   "Mode line construct for the current playing time of the track.")
 
-(defvar emms-status-total-playing-time nil
+(defvar emms-state-total-playing-time nil
   "Mode line construct for the total playing time of the track.")
 
-(defun emms-status-format-time (time)
+(defun emms-state-format-time (time)
   "Convert TIME into a human readable string.
 TIME is a number of seconds."
   (let* ((minutes (/ time 60))
@@ -116,82 +115,82 @@ TIME is a number of seconds."
         (format "%d:%02d" minutes seconds)
       (format "%d:%02d:%02d" hours minutes seconds))))
 
-(defun emms-status-state ()
+(defun emms-state ()
   "Return string displaying the state of the current EMMS process."
   (if emms-player-playing-p
       (if emms-player-paused-p
-          emms-status-pause
-        emms-status-play)
-    emms-status-stop))
+          emms-state-pause
+        emms-state-play)
+    emms-state-stop))
 
-(defun emms-status-set-state ()
-  "Update the value of `emms-status-state' variable."
-  (setq emms-status-state (emms-status-state)))
+(defun emms-state-set-state ()
+  "Update the value of `emms-state' variable."
+  (setq emms-state (emms-state)))
 
-(defun emms-status-set-total-playing-time (&optional _)
-  "Update the value of `emms-status-total-playing-time' variable.
+(defun emms-state-set-total-playing-time (&optional _)
+  "Update the value of `emms-state-total-playing-time' variable.
 Optional argument is used to be compatible with
 `emms-track-updated-functions'."
   (let ((time (emms-track-get (emms-playlist-current-selected-track)
                               'info-playing-time)))
-    (setq emms-status-total-playing-time
-          (and time (emms-status-format-time time)))))
+    (setq emms-state-total-playing-time
+          (and time (emms-state-format-time time)))))
 
-(defun emms-status-set-current-playing-time ()
-  "Update the value of `emms-status-current-playing-time' variable."
-  (setq emms-status-current-playing-time
+(defun emms-state-set-current-playing-time ()
+  "Update the value of `emms-state-current-playing-time' variable."
+  (setq emms-state-current-playing-time
         (unless (zerop emms-playing-time)
-          (emms-status-format-time emms-playing-time))))
+          (emms-state-format-time emms-playing-time))))
 
 
 ;;; Playing time functions for hooks
 
-(defun emms-status-timer-start ()
+(defun emms-state-timer-start ()
   "Start timer for the current playing time."
   (unless emms-playing-time-display-timer
     (setq emms-playing-time-display-timer
-          (run-at-time t 1 'emms-status-playing-time-step))))
+          (run-at-time t 1 'emms-state-playing-time-step))))
 
-(defun emms-status-timer-stop ()
+(defun emms-state-timer-stop ()
   "Stop timer for the current playing time."
   (emms-cancel-timer emms-playing-time-display-timer)
   (setq emms-playing-time-display-timer nil)
-  (emms-status-playing-time-update))
+  (emms-state-playing-time-update))
 
-(defun emms-status-playing-time-step ()
+(defun emms-state-playing-time-step ()
   "Shift the current playing time by one second."
   (setq emms-playing-time (round (1+ emms-playing-time)))
-  (emms-status-playing-time-update))
+  (emms-state-playing-time-update))
 
-(defun emms-status-playing-time-update ()
+(defun emms-state-playing-time-update ()
   "Update the current playing time in the mode line."
-  (emms-status-set-current-playing-time)
+  (emms-state-set-current-playing-time)
   (force-mode-line-update))
 
-(defun emms-status-playing-time-start ()
+(defun emms-state-playing-time-start ()
   "Start displaying the current playing time."
   (setq emms-playing-time 0)
-  (emms-status-timer-start))
+  (emms-state-timer-start))
 
-(defun emms-status-playing-time-stop ()
+(defun emms-state-playing-time-stop ()
   "Stop displaying the current playing time."
   (setq emms-playing-time 0)
-  (emms-status-timer-stop))
+  (emms-state-timer-stop))
 
-(defun emms-status-playing-time-pause ()
+(defun emms-state-playing-time-pause ()
   "Pause displaying the current playing time."
   (if emms-player-paused-p
-      (emms-status-timer-stop)
-    (emms-status-timer-start)))
+      (emms-state-timer-stop)
+    (emms-state-timer-start)))
 
-(defalias 'emms-status-playing-time-seek 'emms-playing-time-seek)
-(defalias 'emms-status-playing-time-set 'emms-playing-time-set)
+(defalias 'emms-state-playing-time-seek 'emms-playing-time-seek)
+(defalias 'emms-state-playing-time-set 'emms-playing-time-set)
 
 
 ;;; Commands
 
 ;;;###autoload
-(define-minor-mode emms-status-mode
+(define-minor-mode emms-state-mode
   "Minor mode for displaying some EMMS info in the mode line.
 
 This mode is intended to be a substitution for `emms-mode-line'
@@ -199,19 +198,19 @@ and `emms-playing-time'."
   :global t
   (or global-mode-string (setq global-mode-string '("")))
   (let (hook-action activep)
-    (if emms-status-mode
+    (if emms-state-mode
         ;; Turn on.
         (progn
           (setq hook-action 'add-hook
                 activep t)
           (when emms-player-playing-p (emms-mode-line-alter))
-          (emms-status-toggle-mode-line 1))
+          (emms-state-toggle-mode-line 1))
       ;; Turn off.
       (setq hook-action 'remove-hook
             activep nil)
-      (emms-status-playing-time-stop)
+      (emms-state-playing-time-stop)
       (emms-mode-line-restore-titlebar)
-      (emms-status-toggle-mode-line -1))
+      (emms-state-toggle-mode-line -1))
 
     (force-mode-line-update)
     (setq emms-mode-line-active-p activep
@@ -224,55 +223,55 @@ and `emms-playing-time'."
              'emms-mode-line-alter)
 
     (funcall hook-action 'emms-track-updated-functions
-             'emms-status-set-total-playing-time)
+             'emms-state-set-total-playing-time)
     (funcall hook-action 'emms-player-started-hook
-             'emms-status-set-total-playing-time)
+             'emms-state-set-total-playing-time)
 
     (funcall hook-action 'emms-player-started-hook
-             'emms-status-set-state)
+             'emms-state-set-state)
     (funcall hook-action 'emms-player-stopped-hook
-             'emms-status-set-state)
+             'emms-state-set-state)
     (funcall hook-action 'emms-player-finished-hook
-             'emms-status-set-state)
+             'emms-state-set-state)
     (funcall hook-action 'emms-player-paused-hook
-             'emms-status-set-state)
+             'emms-state-set-state)
 
     (funcall hook-action 'emms-player-started-hook
-             'emms-status-playing-time-start)
+             'emms-state-playing-time-start)
     (funcall hook-action 'emms-player-stopped-hook
-             'emms-status-playing-time-stop)
+             'emms-state-playing-time-stop)
     (funcall hook-action 'emms-player-finished-hook
-             'emms-status-playing-time-stop)
+             'emms-state-playing-time-stop)
     (funcall hook-action 'emms-player-paused-hook
-             'emms-status-playing-time-pause)
+             'emms-state-playing-time-pause)
     (funcall hook-action 'emms-player-seeked-functions
-             'emms-status-playing-time-seek)
+             'emms-state-playing-time-seek)
     (funcall hook-action 'emms-player-time-set-functions
-             'emms-status-playing-time-set)))
+             'emms-state-playing-time-set)))
 
-(defun emms-status-toggle-mode-line (&optional arg)
+(defun emms-state-toggle-mode-line (&optional arg)
   "Toggle displaying EMMS status info in the mode line.
 
 With prefix argument ARG, enable status info if ARG is positive,
 disable otherwise.
 
-Unlike `emms-status-mode', this function will just remove
-`emms-status-mode-line-string' from `global-mode-string'.  The
+Unlike `emms-state-mode', this function will just remove
+`emms-state-mode-line-string' from `global-mode-string'.  The
 playing timer will still go on."
   (interactive "P")
   (if (or (and (null arg)
-               (not (memq 'emms-status-mode-line-string
+               (not (memq 'emms-state-mode-line-string
                           global-mode-string)))
           (and arg
                (> (prefix-numeric-value arg) 0)))
       (add-to-list 'global-mode-string
-                   'emms-status-mode-line-string
+                   'emms-state-mode-line-string
                    'append)
     (setq global-mode-string
-          (remove 'emms-status-mode-line-string
+          (remove 'emms-state-mode-line-string
                   global-mode-string)))
   (force-mode-line-update))
 
-(provide 'emms-status)
+(provide 'emms-state)
 
-;;; emms-status.el ends here
+;;; emms-state.el ends here
